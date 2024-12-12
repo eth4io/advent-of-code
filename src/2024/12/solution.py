540600104b0c53fa72600944c56e7ae57f7b7bd3
Ethan Zhang <eth4.rocks@gmail.com>
2024-12-13 06:23:40 +1100

    Add day 12 part 2 wip

--- src/2024/12/solution.py
+++ src/2024/12/solution.py
@@ -33,7 +33,31 @@ class Day(Solution):
 
 
   def part_2(self):
-    pass
+    dic = defaultdict(list)
+    all_visited = set()
+    costs = 0
+    for r, row in enumerate(self.g.grid):
+      for c, value in enumerate(row):
+        cell = (r, c)
+        if cell not in all_visited:
+          visited = set()
+          dfs = [cell]
+          visited.add(cell)
+          peri = 0
+          for cur in dfs:
+            for dir in DIRECTIONS_4:
+              for next in [self.g.get_moved(dir, cur)]:
+                if not self.g.is_in_range(next) or self.g.get(next) != value  and dic[(next, dir-1)] != value:
+                  peri += 1
+                elif self.g.is_in_range(next) and next not in visited and self.g.get(next) == value:
+                  visited.add(next)
+                  dfs.append(next)
+                  dic[(cell, dir)] = self.g.get(next)
+
+          costs += peri * len(visited)
+          all_visited = all_visited.union(visited)
+
+    return costs
 
 
 
@@ -48,5 +72,5 @@ assert_equal(lambda: real.part_1(), 1483212, title=real.title)
 
 # part 2
 print(colourify(Colour.LIGHT_BLUE, '\n------- part 2 -------'))
-assert_equal(lambda: real.part_2(), None, title=real.title)
+assert_equal(lambda: real.part_2(), 1206, title=real.title)
 assert_equal(lambda: real.part_2(), None, title=real.title)
